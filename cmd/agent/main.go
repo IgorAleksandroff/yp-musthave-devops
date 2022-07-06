754db4d5708b4cba714b8b7c9249400d72af306d
Igor Aleksandrov <ivaleksandrov@avito.ru>
2022-07-06 23:08:21 +0300

    MemStats

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"runtime"
+)
+
+type gauge float64
+type counter int64
+
+func main() {
+
+	metrics := runtime.MemStats{}
+	fmt.Println(metrics)
+
+	runtime.ReadMemStats(&metrics)
+	fmt.Println(metrics)
+}
