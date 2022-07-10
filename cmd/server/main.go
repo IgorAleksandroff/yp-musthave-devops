8465f1341774a0336a041c59abd8c45e8cb6ae04
Igor Aleksandrov <ivaleksandrov@avito.ru>
2022-07-10 16:55:24 +0300

    добавил сервер с хендлером

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,46 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type metricGauge struct {
+	typeMetric string
+	value      float64
+}
+
+var metricsStorage = map[string]metricGauge{}
+
+func GaugeHandler(w http.ResponseWriter, r *http.Request) {
+	typeMetric := "gauge"
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+
+	metricName := vars["NAME"]
+	metricValue, err := strconv.ParseFloat(vars["VALUE"], 64)
+	if err != nil {
+		http.Error(w, "can't parse a float64. internal error", http.StatusInternalServerError)
+		return
+	}
+
+	metricsStorage[metricName] = metricGauge{
+		typeMetric: typeMetric,
+		value:      metricValue,
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("update/gauge/{NAME}/{VALUE}", GaugeHandler).Methods(http.MethodPost)
+	http.Handle("/", router)
+
+	log.Println("start server")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/IgorAleksandroff/yp-musthave-devops
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
